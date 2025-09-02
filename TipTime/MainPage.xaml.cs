e43f3d35d6cb79a8c5fc7b06d5286d8ee25b8cde
Roger-Paulo01 <rel.cliente.r@gmail.com>
2025-09-01 22:32:22 -0300

    Projeto TipTime

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double Total = double.Parse(ValorTotalEntry.Text);
+            double Slider = double.Parse(Porcentagem15Btn);
+            Total = Total * (Slider / 100);
+
+        }
+
+        private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
