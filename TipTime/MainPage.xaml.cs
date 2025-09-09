53373b38da44bf5c61de3f2f7c01e26cfcd5cc85
rafaela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-09-08 22:34:08 -0300

    Gorjeta

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -7,6 +7,8 @@
         public MainPage()
         {
             InitializeComponent();
+
+            PorcentagemSlider.Value = 17;
         }
         private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
         {
@@ -30,10 +32,35 @@
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            double Total = double.Parse(ValorTotalEntry.Text);
-            double Slider = double.Parse(Porcentagem15Btn);
-            Total = Total * (Slider / 100);
-
+            try 
+            {
+                double porcentegem = PorcentagemSlider.Value;
+                PorcentagemGorjetaLabel.Text = $"{porcentegem}%";
+                //double gorjeta;
+                double valortotal = double.Parse(ValorTotalEntry.Text);
+                //double valortotal = Convert.ToDouble(ValorTotalEntry.Text);
+                //convert é uma classe
+                double gorjeta = valortotal * (porcentegem / 100);
+                double valorfinal = valortotal + gorjeta;
+                //estamos interpolando uma variável que tem conteudo numero em umastring
+                //ValorGorjetaLabel.Text = $"R$ {gorjeta}";
+                //Orecurso de cima não é muito bom
+                //O recurso de baixo é melhor porque o caracter c entre aspas determina a
+                //correta moeda de acordo com as configurações do sistema operacional e
+                //alem disso faz a aproximação sempre para duas casas decimais porque
+                //sabe que estamos trabalhando valores monetários
+                ValorGorjetaLabel.Text = gorjeta.ToString("c");
+                ValorFinalLabel.Text = valorfinal.ToString("c");
+                //ValorFinalLabel.Text = $"R$ {valorfinal}";
+            }
+            catch (Exception ex)
+            {
+                //Toda vez que uso o comando Console.WriteLine a saída vai para a
+                //console na aba Output
+                Console.WriteLine(ex.ToString() );
+                //O Alert não é eficiente pois quebra a navegação do usuário
+                //DisplayAlert("Erro", "Digite um valor numérico váildo","OK");
+            }
         }
 
         private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
